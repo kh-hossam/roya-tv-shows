63ac31a15f8cc9e39a50616a002d15d047c5472e
Khaled Hossam <kh-hossam@outlook.com>
2024-07-31 00:09:37 +0300

    restrict admin panel access to verified email with @admin.com domain

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+        ]);
 
         $this->call([
             SeriesSeeder::class,
